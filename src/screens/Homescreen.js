2d4a381d55fb85f5a852f49d76b96d8aee9dc0f2
Amitmali77 <amitgithub77@gmail.com>
2023-10-02 17:01:08 +0530

    alert msg added

--- src/screens/Homescreen.js
+++ src/screens/Homescreen.js
@@ -12,7 +12,7 @@ export default function Homescreen() {
             .then(res => {
                 setproducts(res.data);
             }).catch(err => {
-                console.log(err)
+                alert(err)
             })
     }
 
@@ -29,7 +29,7 @@ export default function Homescreen() {
                     getAllProducts();
                 }
             }).catch(err=>{
-                console.log(err)
+                alert(err)
             })
         }      
     }
